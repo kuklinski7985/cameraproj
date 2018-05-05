1dabd014675271b429c9e5c7c979f1cf185a7eb7
kuklinski7985 <anku7985@colorado.edu>
2018-05-04 20:04:53 -0600

    Starting new project

---
+++ presence_sense.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+* Copyright (C) 2018 Andrew Kuklinski
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. This file
+* was created as a personal project to better understand programming.  
+* Andrew Kuklinski is not liable for an misuse of this material.
+*
+*******************************************************************************/
+/**
+ * @file template.c
+ * @brief Brief description
+ * 
+ * In depth description.
+ *
+ * @author Andrew Kuklinski
+ * @date May 5th, 2018
+ * @version 1.0
+ *
+ */
